51da3ae5d65918c5a4e2ab7ea9931bb78c5319c0
Guillaume Giamarchi <guillaume.giamarchi@zenika.com>
2012-02-16 14:29:22 +0100

    refactoring switch for stub mode

--- EmailDSL/src/main/java/com/zenika/email/EmailSenderProvider.groovy
+++ EmailDSL/src/main/java/com/zenika/email/EmailSenderProvider.groovy
@@ -11,27 +11,13 @@ class EmailSenderProvider {
 	
 	private static final String SPRING_CONTEXT_XML_FILE = "com/zenika/email/beans.xml"
 	private static final BeanFactory ctx = new ClassPathXmlApplicationContext(SPRING_CONTEXT_XML_FILE)
-	private static boolean stub = false
-
-	/**
-	 * Whether the stub mode is enabled or disabled
-	 * @param stub
-	 */
-	def static setStub(boolean stub) {
-		this.stub = stub
-	}
 
 	/**
 	 * Get a new instance of type {@link EmailSender}
 	 * @return
 	 */
 	static EmailSender getSender() {
-		if (stub) {
-			ctx.getBean("emailSenderStub")
-		}
-		else {
-			ctx.getBean("emailSender")
-		}
+		ctx.getBean("emailSenderProxy")
 	}
 
 }
--- EmailDSL/src/test/java/com/zenika/email/dsl/test/EmailDslTest.groovy
+++ EmailDSL/src/test/java/com/zenika/email/dsl/test/EmailDslTest.groovy
@@ -8,10 +8,9 @@ import com.zenika.email.dsl.Email
 class EmailDslTest extends GroovyTestCase {
 
 	/**
-	 * Test DSL only (stub mode)
+	 * Test DSL + email sending
 	 */
-	void testEmailDslOnly() {
-		EmailSenderProvider.setStub(true) // Stub mode enabled. Will not send an email.
+	void testSendEmail() {
 		Email.send {
 			from "foo.bar@free.fr"
 			to "guillaume.giamarchi@gmail.com"
@@ -20,48 +19,13 @@ class EmailDslTest extends GroovyTestCase {
 			body "Pas grand chose à dire..."
 			attach "file:/E:/file1.txt"
 			attach "file:/E:/file2.txt"
-		}
-	}
-
-	void testEmailDslOnlyWithOptionalBlock() {
-		EmailSenderProvider.setStub(true) // Stub mode enabled. Will not send an email.
-		Email.send {
-			optional(
-				html:false,
-				important:false,
-				headers:[
-					"Reply-To":"bar.foo@free.fr"	
-				]
-			)
-			from "foo.bar@free.fr"
-			to "guillaume.giamarchi@gmail.com"
-			to "guillaume.giamarchi@free.fr"
-			subject "Ouaiiis !!! Un mail envoyé via un DSL en Groovy"
-			body "Pas grand chose à dire..."
 		}
 	}
 	
 	/**
 	 * Test DSL + email sending
 	 */
-	void testSendEmail() {
-		EmailSenderProvider.setStub(false) // Stub mode disabled. An email will be send.
-		Email.send {
-			from "foo.bar@free.fr"
-			to "guillaume.giamarchi@gmail.com"
-			to "guillaume.giamarchi@free.fr"
-			subject "Ouaiiis !!! Un mail envoyé via un DSL en Groovy"
-			body "Pas grand chose à dire..."
-			attach "file:/E:/file1.txt"
-			attach "file:/E:/file2.txt"
-		}
-	}
-	
-	/**
-	* Test DSL + email sending
-	*/
 	void testSendEmailWithOptionalBlock() {
-		EmailSenderProvider.setStub(false) // Stub mode disabled. An email will be send.
 		Email.send {
 			optional(
 				html:true,
